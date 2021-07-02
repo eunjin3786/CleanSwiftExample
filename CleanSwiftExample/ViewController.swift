7e2c3727e370b0f3aa53b0f390a2d2aedd68344e
jinny <jinny.jo@kakaocorp.com>
2021-07-02 12:31:31 +0900

    First commit

---
+++ CleanSwiftExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSwiftExample
+//
+//  Created by Jinny on 2021/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
